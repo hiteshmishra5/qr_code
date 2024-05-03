f4f5621170f840140e47957992348e0421c2fad5
Hitesh Mishra <85689306+hiteshmishra5@users.noreply.github.com>
2024-05-03 17:20:29 +0530

    Update dashboard.py

--- store/Views/dashboard.py
+++ store/Views/dashboard.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from store.models.customer import Users
-from store.models.Coordinator import Coordinator
 from store.models.location import Location
+from .login import user_type_required
 
+# @user_type_required('coordinator')
 class Dashboard(View):
     def get(self, request):
         email = request.session.get('email')
         password = request.session.get('password')
-        coordinator = Coordinator.objects.filter(email=email, password=password).first()
         customers = Users.objects.order_by('-id')
         location = Location.objects.all()
         unique_dates = set()
@@ -25,74 +25,85 @@ class Dashboard(View):
                 modalities_filter['location_id'] = selected_location_id
 
         count_modalities = {
-                'xray': Users.objects.filter(xray=True, **modalities_filter).count(),
-                'ecg': Users.objects.filter(ecg=True, **modalities_filter).count(),
-                'pft': Users.objects.filter(pft=True, **modalities_filter).count(),
-                'audiometry': Users.objects.filter(audiometry=True, **modalities_filter).count(),
-                'optometry': Users.objects.filter(optometry=True, **modalities_filter).count(),
-                'sputum': Users.objects.filter(sputum=True, **modalities_filter).count(),
-                'sample_collection': Users.objects.filter(sample_collection=True, **modalities_filter).count()
+            'registration': Users.objects.filter(registration=True, **modalities_filter).count(),
+            'xray': Users.objects.filter(xray=True, **modalities_filter).count(),
+            'ecg': Users.objects.filter(ecg=True, **modalities_filter).count(),
+            'pft': Users.objects.filter(pft=True, **modalities_filter).count(),
+            'audiometry': Users.objects.filter(audiometry=True, **modalities_filter).count(),
+            'optometry': Users.objects.filter(optometry=True, **modalities_filter).count(),
+            'vitals': Users.objects.filter(vitals=True, **modalities_filter).count(),
+            'sputum': Users.objects.filter(sputum=True, **modalities_filter).count(),
+            'sample_collection': Users.objects.filter(sample_collection=True, **modalities_filter).count(),
+            'pathology': Users.objects.filter(pathology=True, **modalities_filter).count(),
         }
 
         data = {
-                'coordinator': coordinator,
                 'customers': customers,
                 'total_patient_counts': total_patients_count,
                 'count': count_modalities,
                 'locations': location,
-                'date': formatted_dates  # You need to define formatted_dates
+                'date': formatted_dates
         }
 
         return render(request, 'dashboard.html', {'data': data})
-        # xray_modalities = Users.objects.filter(xray=True).count()
-        # ecg_modalities = Users.objects.filter(ecg=True).count()
-        # pft_modalities = Users.objects.filter(pft=True).count()
-        # audiometry_modalities = Users.objects.filter(audiometry=True).count()
-        # optometry_modalities = Users.objects.filter(optometry=True).count()
-        # sputum_modalities = Users.objects.filter(sputum=True).count()
-        # sample_collection_modalities = Users.objects.filter(sample_collection=True).count()
         
         if selected_location_id==1:
+                registration = Users.objects.filter(location_id=1, registration=True).count()
                 xray_modalities = Users.objects.filter(location_id=1, xray=True).count()
                 ecg_modalities = Users.objects.filter(location_id=1, ecg=True).count()
                 pft_modalities = Users.objects.filter(location_id=1, pft=True).count()
                 audiometry_modalities = Users.objects.filter(location_id=1, audiometry=True).count()
                 optometry_modalities = Users.objects.filter(location_id=1, optometry=True).count()
+                vitals_modalities = Users.objects.filter(location_id=1, vitals=True).count()
                 sputum_modalities = Users.objects.filter(location_id=1, sputum=True).count()
                 sample_collection_modalities = Users.objects.filter(location_id=1, sample_collection=True).count()
+                pathology = Users.objects.filter(location_id=1, sputum=True).count()
 
         elif selected_location_id==2:
+                registration = Users.objects.filter(location_id=2, registration=True).count()
                 xray_modalities = Users.objects.filter(location_id=2, xray=True).count()
                 ecg_modalities = Users.objects.filter(location_id=2, ecg=True).count()
                 audiometry_modalities = Users.objects.filter(location_id=2, audiometry=True).count()
                 optometry_modalities = Users.objects.filter(location_id=2, optometry=True).count()
+                vitals_modalities = Users.objects.filter(location_id=2, vitals=True).count()
                 sputum_modalities = Users.objects.filter(location_id=2, sputum=True).count()
                 sample_collection_modalities = Users.objects.filter(location_id=2, sample_collection=True).count()
+                pathology = Users.objects.filter(location_id=1, sputum=True).count()
 
         elif selected_location_id==3:
+                registration = Users.objects.filter(location_id=3, registration=True).count()
                 xray_modalities = Users.objects.filter(location_id=3, xray=True).count()
                 ecg_modalities = Users.objects.filter(location_id=3, ecg=True).count()
                 audiometry_modalities = Users.objects.filter(location_id=3, audiometry=True).count()
                 optometry_modalities = Users.objects.filter(location_id=3, optometry=True).count()
+                vitals_modalities = Users.objects.filter(location_id=3, vitals=True).count()
                 sputum_modalities = Users.objects.filter(location_id=3, sputum=True).count()
                 sample_collection_modalities = Users.objects.filter(location_id=3, sample_collection=True).count()
+                pathology = Users.objects.filter(location_id=1, sputum=True).count()
         
         elif  selected_location_id==4:
+                registration = Users.objects.filter(location_id=4, registration=True).count()
                 xray_modalities = Users.objects.filter(location_id=4, xray=True).count()
                 ecg_modalities = Users.objects.filter(location_id=4, ecg=True).count()
                 audiometry_modalities = Users.objects.filter(location_id=4, audiometry=True).count()
                 optometry_modalities = Users.objects.filter(location_id=4, optometry=True).count()
+                vitals_modalities = Users.objects.filter(location_id=4, vitals=True).count()
                 sputum_modalities = Users.objects.filter(location_id=4, sputum=True).count()
                 sample_collection_modalities = Users.objects.filter(location_id=4, sample_collection=True).count()
+                pathology = Users.objects.filter(location_id=1, sputum=True).count()
         
         else :
+                registration = Users.objects.filter(location_id=5, registration=True).count()
                 xray_modalities = Users.objects.filter(xray=True).count()
                 ecg_modalities = Users.objects.filter(ecg=True).count()
                 pft_modalities = Users.objects.filter(pft=True).count()
                 audiometry_modalities = Users.objects.filter(audiometry=True).count()
                 optometry_modalities = Users.objects.filter(optometry=True).count()
+                vitals_modalities = Users.objects.filter(vitals=True).count()
                 sputum_modalities = Users.objects.filter(sputum=True).count()
                 sample_collection_modalities = Users.objects.filter(sample_collection=True).count()
+                pathology = Users.objects.filter(location_id=1, sputum=True).count()
+
                
 
         count_modalities = {'xray': xray_modalities,
@@ -100,11 +111,13 @@ class Dashboard(View):
                             'pft': pft_modalities,
                             'audiometry': audiometry_modalities,
                             'optometry': optometry_modalities,
+                            'vitals': vitals_modalities,
                             'sputum': sputum_modalities,
-                            'sample_collection': sample_collection_modalities}
+                            'sample_collection': sample_collection_modalities,
+                            'pathology': pathology,
+                            'registration': registration}
 
-
-        data = {'coordinator': coordinator, 'customers': customers, 'total_patient_counts': total_patients_count,
+        data = {'customers': customers, 'total_patient_counts': total_patients_count,
                 'count': count_modalities, 'locations': location, 'date': formatted_dates}
         
         return render(request, 'dashboard.html', {'data': data})
