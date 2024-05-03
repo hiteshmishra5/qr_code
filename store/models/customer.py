5e88ddeaad95a1b270298a966aff949140baad74
Hitesh Mishra <85689306+hiteshmishra5@users.noreply.github.com>
2024-05-03 17:17:33 +0530

    Update customer.py

--- store/models/customer.py
+++ store/models/customer.py
@@ -4,16 +4,17 @@ from django.utils import timezone
 
 class Users(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, default=None, related_name='users')
-    patient_id = models.CharField(max_length=100, unique=True, null=True)
+    xrai_id = models.CharField(max_length=100, default = '0')
+    patient_id = models.CharField(max_length=100, default='0')
     patient_name = models.CharField(max_length=100, null=True)
     age = models.IntegerField(null=True, default=None, blank=True)
     gender = models.CharField(max_length=15, null=True)
     phone = models.CharField(max_length=10, null=True, default=None, blank=True)
     email = models.CharField(max_length=50, null=True, blank=True)
-    weight = models.IntegerField(null=True, default=None)
     date_field = models.DateField(default=timezone.now)
     current_time = models.DateTimeField(auto_now_add=True)
 
+    registration = models.BooleanField(default=False)
     xray = models.BooleanField(default=False)
     ecg = models.BooleanField(default=False)
     pft = models.BooleanField(default=False)
@@ -22,6 +23,7 @@ class Users(models.Model):
     sputum = models.BooleanField(default=False)
     vitals = models.BooleanField(default=False)
     sample_collection = models.BooleanField(default=False)
+    pathology = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.patient_name)
